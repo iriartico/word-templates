ebd6f90f8680df0a212e682ded757ba95de5acf5
Jose Iriarte <joseiriarte2102@gmail.com>
2025-01-06 17:43:02 -0400

    Certificados Docentes

--- server.py
+++ server.py
@@ -34,29 +34,7 @@ def convert_date(value):
         return value
 
 
-@app.route("/generar-carta-invitacion", methods=["POST"])
-def generar_carta():
-    data = request.json
-    print(data)
-    if not data:
-        return jsonify({"error": "No se enviaron datos."}), 400
-
-    doc = DocxTemplate("docs/carta-invitacion.docx")
-
-    hoy = f"{datetime.now().day} de {meses[datetime.now().month]} de {datetime.now().year}"
-    data["hoy"] = hoy
-    data["fecha_clase_1"] = convert_date(data.get("fecha_clase_1"))
-    data["fecha_clase_2"] = convert_date(data.get("fecha_clase_2"))
-    data["fecha_clase_3"] = convert_date(data.get("fecha_clase_3"))
-    data["fecha_clase_4"] = convert_date(data.get("fecha_clase_4"))
-    data["fecha_clase_5"] = convert_date(data.get("fecha_clase_5"))
-    data["fecha_clase_6"] = convert_date(data.get("fecha_clase_6"))
-
-    doc.render(data)
-
-    docente = data.get("docente", "Desconocido").replace(".", "").title()
-    file_name = f"Carta Invitacion - {docente}.docx"
-
+def download_file(doc, file_name):
     with tempfile.TemporaryDirectory() as tmp_dir:
         file_path = os.path.join(tmp_dir, file_name)
         pdf_path = file_path.replace(".docx", ".pdf")
@@ -78,6 +56,49 @@ def generar_carta():
         return send_from_directory(
             tmp_dir, os.path.basename(pdf_path), as_attachment=True
         )
+
+
+@app.route("/generar-carta-invitacion", methods=["POST"])
+def generar_carta_invitacion():
+    data = request.json
+    if not data:
+        return jsonify({"error": "No se enviaron datos."}), 400
+
+    doc = DocxTemplate("docs/carta-invitacion.docx")
+
+    hoy = f"{datetime.now().day} de {meses[datetime.now().month]} de {datetime.now().year}"
+    data["hoy"] = hoy
+    for i in range(1, 7):
+        data[f"fecha_clase_{i}"] = convert_date(data.get(f"fecha_clase_{i}"))
+
+    doc.render(data)
+
+    docente = data.get("docente", "Desconocido").replace(".", "").title()
+    file_name = f"Carta Invitacion - {docente}.docx"
+
+    return download_file(doc, file_name)
+
+
+@app.route("/generar-certificado-docente", methods=["POST"])
+def generar_certificado_docente():
+    data = request.json
+    # print(data)
+    if not data:
+        return jsonify({"error": "No se enviaron datos."}), 400
+
+    doc = DocxTemplate("docs/certificado-docente.docx")
+
+    hoy = f"{datetime.now().day} de {meses[datetime.now().month]} de {datetime.now().year}"
+    data["hoy"] = hoy
+    data["fecha_inicio"] = convert_date(data.get("fecha_inicio"))
+    data["fecha_fin"] = convert_date(data.get("fecha_fin"))
+
+    doc.render(data)
+
+    docente = data.get("docente", "Desconocido").replace(".", "").title()
+    file_name = f"Certificado Docente - {docente}.docx"
+
+    return download_file(doc, file_name)
 
 
 if __name__ == "__main__":
